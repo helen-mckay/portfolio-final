cfcb06365af3904cb58c7fd8a67426dbe33b764e
Helen McKay <helen.mckay@comcast.net>
2020-11-13 16:17:45 -0600

    connected firebase and tested with react-firebase-hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 function App() {
+
+  const [user, user_loading, user_error] = useAuthState(
+    firebase.auth()
+  );
+
+  const logout = () => {
+    firebase.auth().signOut();
+  }
+
+  const login = () => {
+    firebase.auth().signInWithEmailAndPassword("helenmckaypersonal@gmail.com", "THISISMYWEBSITE");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? <button onClick={logout}>log out</button> : <button onClick={login}>log in</button>}
+      {user_loading && <p>loading...</p>}
+      {user && <p>Hello handsome</p>}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+
+// NOTE: move keys to .env file before pushing to github
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+if (firebase.apps.length === 0)
+{
+    firebase.initializeApp(firebaseConfig);
+}
+
+
+export default firebase;
